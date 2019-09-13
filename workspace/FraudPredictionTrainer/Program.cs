f834885633b505c0942295a7a622fb65d062d10f
Rob Levine <rob@roblevine.co.uk>
2019-09-13 10:16:03 +0100

    2.3 Load your data in ML.NET

--- workspace/FraudPredictionTrainer/Program.cs
+++ workspace/FraudPredictionTrainer/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Microsoft.ML;
 
 namespace FraudPredictionTrainer
 {
     class Program
     {
+        private static string DataPath = "data.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mlContext = new MLContext(seed: 1);
+             var data = mlContext.Data.LoadFromTextFile<Transaction>(DataPath, hasHeader: true, separatorChar: ',');
+
         }
     }
 }
